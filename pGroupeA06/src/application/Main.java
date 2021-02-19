d86cb12b6835bb3b7b20e8cda967ff281cd0e140
damie <damie@DESKTOP-SSA7UP8>
2021-02-19 19:03:25 +0100

    add icon

--- pGroupeA06/src/application/Main.java
+++ pGroupeA06/src/application/Main.java
@@ -4,15 +4,21 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import vue.AddCards;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import icon.*;
+
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			
 			AddCards root = new AddCards();
-
+			primaryStage.setTitle("How much are you worth ?");
+			primaryStage.getIcons().add(new Image("/icon/icon.png"));
+			
 			Scene scene = new Scene(root,600,315);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
