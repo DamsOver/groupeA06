fefaeb2df0f38f89cc5fa682b37c4e1ab9f2b10e
damie <damie@DESKTOP-SSA7UP8>
2021-03-03 20:55:25 +0100

    change Admin

--- pGroupeA06/src/vue/AdminAP.java
+++ pGroupeA06/src/vue/AdminAP.java
@@ -6,6 +6,7 @@ import javafx.event.EventHandler;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -13,61 +14,80 @@ import javafx.scene.text.Text;
 
 public class AdminAP extends AnchorPane {
 
-	private Text txtTitle;
-	private Button btnBack;
-	private Button btnUnlock;
+	private Text txtTitle, txtLogin, txtPassword;
+	private TextField txtFLogin, txtFPassword;
+
+	private Button btnBack, btnUnlock;
 
 	public AdminAP() {
-		
 		this.getStyleClass().add("pane");
-		this.getChildren().addAll(getTxtTitle(), getBtnBack(),getBtnUnlock());
-		
+		this.getChildren().addAll(getTxtTitle(), getBtnBack(), getBtnUnlock(), getTxtPassword(), getTxtLogin(),
+				getTxtFLogin(), getTxtFPassword());
+
 		// title
 		txtTitle.getStyleClass().add("title-style");
 		AnchorPane.setTopAnchor(getTxtTitle(), 100.0);
-		AnchorPane.setRightAnchor(getTxtTitle(), 700.0);
 		AnchorPane.setLeftAnchor(getTxtTitle(), 700.0);
-		
-		// title
-		txtTitle.getStyleClass().add("title-style");
-		AnchorPane.setTopAnchor(getTxtTitle(), 100.0);
-		AnchorPane.setRightAnchor(getTxtTitle(), 700.0);
-		AnchorPane.setLeftAnchor(getTxtTitle(), 700.0);
-		
+
+		// Login
+		txtLogin.getStyleClass().add("basicText");
+		AnchorPane.setTopAnchor(getTxtLogin(), 450.0);
+		AnchorPane.setLeftAnchor(getTxtLogin(), 200.0);
+
+		// Password
+		txtPassword.getStyleClass().add("basicText");
+		AnchorPane.setTopAnchor(getTxtPassword(), 550.0);
+		AnchorPane.setLeftAnchor(getTxtPassword(), 200.0);
+
+		// LoginField
+		txtFLogin.getStyleClass().add("txtField");
+		AnchorPane.setTopAnchor(getTxtFLogin(), 445.0);
+		AnchorPane.setLeftAnchor(getTxtFLogin(), 410.0);
+		AnchorPane.setRightAnchor(getTxtFLogin(), 1160.0);
+
+		// PasswordField
+		txtFPassword.getStyleClass().add("txtField");
+		AnchorPane.setTopAnchor(getTxtFPassword(), 545.0);
+		AnchorPane.setLeftAnchor(getTxtFPassword(), 410.0);
+		AnchorPane.setRightAnchor(getTxtFPassword(), 1160.0);
+
+		// Unlock
+		btnUnlock.getStyleClass().add("btn-style");
+		AnchorPane.setTopAnchor(getBtnUnlock(), 700.0);
+		AnchorPane.setLeftAnchor(getBtnUnlock(), 200.0);
+
 		// BtnBack
 		btnBack.getStyleClass().add("btn-style");
 		AnchorPane.setTopAnchor(getBtnBack(), 900.0);
-		AnchorPane.setRightAnchor(getBtnBack(), 50.0);
 		AnchorPane.setLeftAnchor(getBtnBack(), 1600.0);
-		
-//		this.getStyleClass().add("pane");
-//		this.setPadding(new Insets(10));
-////		this.setGridLinesVisible(true); // Rend visible ligne de séparation
-//		this.setVgap(10);
-//		this.setHgap(10);
-//
-//		int nbCol = 20;
-//		for (int i = 0; i < nbCol; i++) {
-//			ColumnConstraints colConstr = new ColumnConstraints();
-//			colConstr.setPercentWidth(100. / nbCol);
-//			this.getColumnConstraints().add(colConstr);
-//		}
-//
-//		// Line 2
-//		this.add(getTxtTitle(), 8, 1, 4, 1); // elt, columnIndex, rowIndex, colspan, rowspan
-//		txtTitle.setText("Admin");
-//		GridPane.setHalignment(getTxtTitle(), HPos.CENTER);
-//		txtTitle.getStyleClass().add("title-style");
-//
-//		// Line 4
-//		this.add(getBtnAddCards(), 7, 3, 6, 1);
-//		GridPane.setHalignment(getBtnAddCards(), HPos.CENTER);
-//		addCards.getStyleClass().add("btn-style");
-//
-//		// Line 20
-//		this.add(getBtnBack(), 18, 15, 4, 3);
-//		btnBack.getStyleClass().add("btn-style");
+	}
 
+	public Text getTxtLogin() {
+		if (txtLogin == null) {
+			txtLogin = new Text("Login : ");
+		}
+		return txtLogin;
+	}
+
+	public Text getTxtPassword() {
+		if (txtPassword == null) {
+			txtPassword = new Text("Password : ");
+		}
+		return txtPassword;
+	}
+
+	public TextField getTxtFLogin() {
+		if (txtFLogin == null) {
+			txtFLogin = new TextField();
+		}
+		return txtFLogin;
+	}
+
+	public TextField getTxtFPassword() {
+		if (txtFPassword == null) {
+			txtFPassword = new TextField();
+		}
+		return txtFPassword;
 	}
 
 	public Text getTxtTitle() {
@@ -81,10 +101,10 @@ public class AdminAP extends AnchorPane {
 		if (btnBack == null) {
 			btnBack = new Button("Back");
 		}
-		btnBack.setOnAction(new EventHandler<ActionEvent>(){
-	        public void handle(ActionEvent event) {
-	            SceneManager.getSceneRoot().setRoot(SceneManager.getStackOptions());
-	        }
+		btnBack.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				SceneManager.getSceneRoot().setRoot(SceneManager.getStackOptions());
+			}
 		});
 		return btnBack;
 	}
@@ -93,6 +113,12 @@ public class AdminAP extends AnchorPane {
 		if (btnUnlock == null) {
 			btnUnlock = new Button("Unlock");
 		}
+		btnBack.setOnAction(new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				SceneManager.getSceneRoot().setRoot(SceneManager.getStackOptions());
+			}
+		});
+		
 		return btnUnlock;
 	}
 
